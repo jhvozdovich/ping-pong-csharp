a9c5f82366772b1ecd8d60dc9c2084c6c3b27fc1
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-04 10:03:48 -0700

    add pingpong logic

--- ping-pong.cs
+++ ping-pong.cs
@@ -1,8 +1,35 @@
 using System;
+using System.Collections.Generic;
 class PingPong
 {
   static void Main()
   {
-    
+    Console.WriteLine("Enter a number for ping pong!");
+    string stringNumEntry = Console.ReadLine();
+    int numEntry = int.Parse(stringNumEntry);
+    List<string> numList = new List<string> { };
+    for (int i = 1; i <= numEntry; i++)
+    {
+      if (i % 15 == 0)
+      {
+        numList.Add("ping-pong");
+      }
+      else if (i % 3 == 0)
+      {
+        numList.Add("ping");
+      }
+      else if (i % 5 == 0)
+      {
+        numList.Add("pong");
+      }
+      else
+      {
+        numList.Add(i.ToString());
+      }
+    }
+    foreach (string i in numList)
+    {
+      Console.WriteLine(i);
+    }
   }
 }
